ad11dcf6dd93685228ab14b59ad0d4bcde72c81d
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-10-29 20:23:09 -0600

    Make hero section responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,16 +6,20 @@ export default function Hero() {
     <section className="Hero">
       <div>
         <h1>
-          The time to get <span>strong</span> has come
+          The time to get <span>strong</span>&nbsp;has come
         </h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra amet
-          placerat mauris elementum dignissim.
+        <p className="hero-paragraph">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+          <span>Viverra amet placerat mauris elementum dignissim.</span>
         </p>
         <button>Contact Us</button>
       </div>
       <div>
-        <img alt="man working out" src={workoutVector1} />
+        <img
+          alt="man working out"
+          className="workoutVector1"
+          src={workoutVector1}
+        />
       </div>
     </section>
   );
