7723551ccebcc5f68a44b4638da87f2b6d30cff8
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-10-30 12:38:35 -0600

    Make the nav links responsive and add an evenly space

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,18 @@ import "./Header.css";
 export default function Header() {
   return (
     <nav>
-      <h2>
-        Trainer<span className="header-span">Hub</span>
-      </h2>
+      <div className="logo-container">
+        <h2>
+          Trainer<span className="header-span">Hub</span>
+        </h2>
+      </div>
+      <div className="nav-links-container">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact</li>
+        </ul>
+      </div>
     </nav>
   );
 }
