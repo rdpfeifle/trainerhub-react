c4abc86f09572a23eed32597237e481cd5e0029d
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-11-01 16:35:38 -0600

    Create Benefits component layout and render in Home.jsx

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,3 +1,44 @@
+import "./Benefits.css";
+import workoutVector3 from "../assets/workout-vector-3.webp";
+
 export default function Benefits() {
-  return <div></div>;
+  return (
+    <section className="Benefits">
+      <img src={workoutVector3} alt="lady working out" />
+      <h2>
+        <span>The benefits</span> of exercise
+      </h2>
+      <div className="cards-container">
+        <div className="card">
+          <h3>Increases business flexibility</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra
+            amet placerat mauris elementum dignissim.
+          </p>
+        </div>
+        <div className="card">
+          <h3>Improves mental health</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra
+            amet placerat mauris elementum dignissim.
+          </p>
+        </div>
+        <div className="card">
+          <h3>Provides more energy throughout the day</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra
+            amet placerat mauris elementum dignissim.
+          </p>
+        </div>
+        <div className="card">
+          <h3>Helps to find new friends</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra
+            amet placerat mauris elementum dignissim.
+          </p>
+        </div>
+      </div>
+      <button className="benefits-btn">Get now</button>
+    </section>
+  );
 }
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,6 +2,7 @@ import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Details from "../components/Details";
 import About from "../components/About";
+import Benefits from "../components/Benefits";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <Details />
       <About />
+      <Benefits />
     </>
   );
 }
